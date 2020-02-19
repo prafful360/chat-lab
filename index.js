81532982421baf00e2dbb82ba6a447a42b869891
prafful360 <praffulranjan54@gmail.com>
2020-02-19 20:31:33 +0530

    __

--- index.js
+++ index.js
@@ -1,19 +1,70 @@
-
-var express = require("express");
+var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
-const messages = {
-    name:'John',
-    message: "Hello"
-}
-app.use(express.static(__dirname))
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
-app.get('/messages', (req,res)=>{
-    res.send(messages)
+var Message = mongoose.model('Message',{
+  name : String,
+  message : String
 })
 
-var PORT = 3000;
-var server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    
+var dbUrl = 'mongodb://amkurian:amkurian1@ds257981.mlab.com:57981/simple-chat'
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
 })
+
+
+app.get('/messages/:user', (req, res) => {
+  var user = req.params.user
+  Message.find({name: user},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+
+app.post('/messages', async (req, res) => {
+  try{
+    var message = new Message(req.body);
+
+    var savedMessage = await message.save()
+      console.log('saved');
+
+    var censored = await Message.findOne({message:'badword'});
+      if(censored)
+        await Message.remove({_id: censored.id})
+      else
+        io.emit('message', req.body);
+      res.sendStatus(200);
+  }
+  catch (error){
+    res.sendStatus(500);
+    return console.log('error',error);
+  }
+  finally{
+    console.log('Message Posted')
+  }
+
+})
+
+
+
+io.on('connection', () =>{
+  console.log('a user is connected')
+})
+
+mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
+  console.log('mongodb connected',err);
+})
+
+var server = http.listen(3000, () => {
+  console.log('server is running on port', server.address().port);
+});
