1101c7aab5922ce1092751abd2385bce4e717e43
prafful360 <praffulranjan54@gmail.com>
2020-02-20 17:54:55 +0530

    New Approach

--- index.js
+++ index.js
@@ -1,70 +1,46 @@
-var express = require('express');
-var bodyParser = require('body-parser')
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var mongoose = require('mongoose');
+const express = require("express");
 
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
+const app = express();
+const path = require("path")
+const io = require('socket.io')();
 
-var Message = mongoose.model('Message',{
-  name : String,
-  message : String
-})
-
-var dbUrl = 'mongodb://amkurian:amkurian1@ds257981.mlab.com:57981/simple-chat'
-
-app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
-    res.send(messages);
-  })
+app.listen(80, ()=>{
+    console.log("Server is running at port 80");
+    io.listen(3000)    
 })
 
 
-app.get('/messages/:user', (req, res) => {
-  var user = req.params.user
-  Message.find({name: user},(err, messages)=> {
-    res.send(messages);
-  })
+app.set('views','./views');
+app.set('views', path.join(__dirname, 'views'));
+
+// Set view engine as EJS
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
+app.get('/', (req,res) =>{
+    res.render('index.html');
 })
+var users = {};
+function createUser(name, clientId){
+    return{
+        name: name,
+        clientId: clientId,
+        x:250,
+        y:250,
+        message: ''
+    }
+}
 
-
-app.post('/messages', async (req, res) => {
-  try{
-    var message = new Message(req.body);
-
-    var savedMessage = await message.save()
-      console.log('saved');
-
-    var censored = await Message.findOne({message:'badword'});
-      if(censored)
-        await Message.remove({_id: censored.id})
-      else
-        io.emit('message', req.body);
-      res.sendStatus(200);
-  }
-  catch (error){
-    res.sendStatus(500);
-    return console.log('error',error);
-  }
-  finally{
-    console.log('Message Posted')
-  }
-
+io.on('connection', client => {
+    client.on('login',name =>{
+        var user = createUser(name, client.id);
+        users[client.id] = user;
+        client.broadcast.emit('user-connected',user)
+        client.emit('users-list', users)
+    })
+    client.on('disconnect', () => {
+        delete users[client.id];
+        client.broadcast.emit('User-disconnected', client.id)
+    })
 })
-
-
-
-io.on('connection', () =>{
-  console.log('a user is connected')
-})
-
-mongoose.connect(dbUrl ,{useMongoClient : true} ,(err) => {
-  console.log('mongodb connected',err);
-})
-
-var server = http.listen(3000, () => {
-  console.log('server is running on port', server.address().port);
-});
